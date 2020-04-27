273eb7285680224f4e0b35615c0af393e0088084
enricovc2001 <enricov.costa01@gmail.com>
2020-04-27 18:29:53 -0300

    dropdown criado, faltando interação

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
@@ -3,12 +3,45 @@ package br.pro.hashi.ensino.desagil.projeto1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.BreakIterator;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.text);
+
+        Spinner dropdownSpinner = findViewById(R.id.dropdown);
+        dropdownSpinner.setOnItemSelectedListener(this);
+
+        ArrayAdapter<CharSequence> adapterDropdown = ArrayAdapter.createFromResource(this,
+                R.array.frases, android.R.layout.simple_spinner_item);
+
+        adapterDropdown.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        dropdownSpinner.setAdapter(adapterDropdown);
+
+
+
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
 }
